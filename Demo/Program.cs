76270b414214d65f7e0ffba145369e16e3bbbd4b
Lucas Vu <lucas.vu@qorvo.com>
2020-06-10 18:49:50 +0700

    Add oled class (not done yet)

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿#define FT2232H
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using FTD2XX_NET;
+
 
 namespace Demo
 {
